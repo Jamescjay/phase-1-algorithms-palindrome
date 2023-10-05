aef85395959e43cab11f5fffa4958ad0e0190cb9
James <james.ekasiba@student.moringaschool.com>
2023-10-04 20:21:40 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+    word = word.replace(/[^a-zA-Z0-9]/g, "");
+
+    return word === word.split("").reverse().join("");
+  }
+
 
 /* 
-  Add your pseudocode here
+  Write a function isPalindrome that will receive one argument, a string
+
+inside the function: Remove non-alphanumeric characters and  Compare the string with its reverse
+  if the string passed is equal to its reverse return true
+    else return false
+
 */
 
 /*
-  Add written explanation of your solution here
+  my function receives a string as an argument and then compares it with its revese and
+  returns true if the string passed is equal to its revese.
+  It returns false if the string is not the same as its reverse.
+  Thus my function test whether the string passed is a Palindrome.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +27,7 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("=>", isPalindrome(mom));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
